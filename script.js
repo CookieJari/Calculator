28c58f13fe3b9c75804297885317967c2ebe3f16
Jari <zalmanhajari@gmail.com>
2023-03-14 17:49:17 +0800

    Finished the basic button layout

---
+++ script.js
@@ -0,0 +1,26 @@
+var n1=0;
+var n2=0;
+
+var n1str;
+var n2str;
+const textDiv = document.querySelector(".grid-item-text");
+
+const btn1 = document.querySelector(".grid-item-1");
+const btn2 = document.querySelector(".grid-item-2");
+const btn3 = document.querySelector(".grid-item-3");
+const btn4 = document.querySelector(".grid-item-4");
+const btn5 = document.querySelector(".grid-item-5");
+const btn6 = document.querySelector(".grid-item-6");
+const btn7 = document.querySelector(".grid-item-7");
+const btn8 = document.querySelector(".grid-item-8");
+const btn9 = document.querySelector(".grid-item-9");
+const btn0 = document.querySelector(".grid-item0");
+
+const btnDiv = document.querySelector(".grid-item-div");
+const btnMul = document.querySelector(".grid-item-mul");
+const btnSub = document.querySelector(".grid-item-sub");
+const btnAdd = document.querySelector(".grid-item-add");
+
+textDiv.textContent = "helo"
+console.log(textDiv.textContent);
+
